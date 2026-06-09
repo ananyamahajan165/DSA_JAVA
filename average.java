319528388d0545a1e838a7e7e4930a597b711973
Your Name <ananyam054@gmail.com>
2026-06-09 14:05:07 +0530

    Basic average

--- average.java
+++ average.java
@@ -2,18 +2,19 @@ import java.util.Scanner;
 
 public class average {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
-        while(t-->0){
-            int a = scn.nextInt();
-            int b = scn.nextInt();
-            int c = scn.nextInt();
-            if((a+b)>=70 && (a+c)>=70 && (b+c)>=70){
-                System.out.println("PASS");
-            }
-            else{
-                System.out.println("FAIL");
+        try (Scanner scn = new Scanner(System.in)) {
+            int t = scn.nextInt();
+            while (t-- > 0) {
+                int a = scn.nextInt();
+                int b = scn.nextInt();
+                int c = scn.nextInt();
+                if ((a + b) >= 70 && (a + c) >= 70 && (b + c) >= 70) {
+                    System.out.println("PASS");
+                } else {
+                    System.out.println("FAIL");
+                }
             }
         }
     }
 }
+
