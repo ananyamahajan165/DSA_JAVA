f5f338c0dc45792e63e5dc50439d3522f2809123
Your Name <ananyam054@gmail.com>
2026-07-20 01:39:50 +0530

    feat: add LeetCode 53 Maximum Subarray (Kadane's Algorithm)

---
+++ MaximumSubarray.java
@@ -0,0 +1,15 @@
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int sum=0;
+        int n=nums.length;
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            max=Math.max(max,sum);
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return max;
+    }
+}
