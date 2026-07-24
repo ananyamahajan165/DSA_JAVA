e6bf125bd8d551c7c55c21a67552ad6915c1723a
Your Name <ananyam054@gmail.com>
2026-07-24 10:37:03 +0530

    feat: add LeetCode 2761 Prime Pairs With Target Sum using Sieve and two pointers

---
+++ PrimePairsWithTargetSum.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PrimePairsWithTargetSum {
+    int N = 1000000;
+    int[] prime = new int[N];
+    public void sieve(){
+        Arrays.fill(prime,1);
+        prime[0] = 0;
+        prime[1] = 1;
+        for(int i=2;i*i<N;i++){
+            if(prime[i]==1){
+                for(int j=i*i;j<N;j+=i){
+                    prime[j]=0;
+                }
+            }
+        }
+    }
+    public List<List<Integer>> findPrimePairs(int n) {
+        sieve();
+        int l=2;
+        int r=n-1;
+        List<List<Integer>> ans = new ArrayList<>();
+        while(l<=r){
+            if(prime[l]==0){
+                l++;
+                continue;
+            }
+            else if(prime[r]==0){
+                r--;
+                continue;
+            }
+            else if(l+r==n){
+                List<Integer> temp = new ArrayList<>();
+                temp.add(l);
+                temp.add(r);
+                ans.add(temp);
+                l++;
+                r--;
+            }
+            else if(l+r<n){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return ans;
+    }
+}
