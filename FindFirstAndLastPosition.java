caa6ef05238e1b90644eb574900ceef01c55e49e
Your Name <ananyam054@gmail.com>
2026-07-26 23:30:56 +0530

    feat: add LeetCode 34 Find First and Last Position using binary search

---
+++ FindFirstAndLastPosition.java
@@ -0,0 +1,30 @@
+public class FindFirstAndLastPosition {
+    public int search(int[] arr, int t, int first) {
+        int s = 0;
+        int e = arr.length - 1;
+        int ans = -1;
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+            if (arr[m] == t) {
+                ans = m;
+                if (first == 1) {
+                    e = m - 1; 
+                } else {
+                    s = m + 1;
+                }
+            } else if (arr[m] > t) {
+                e = m - 1;
+            } else {
+                s = m + 1;
+            }
+        }
+        return ans;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans=new int[2];
+        ans[0] = search(nums, target, 1);
+        ans[1] = search(nums, target, 2);
+        return ans;
+    }
+}
