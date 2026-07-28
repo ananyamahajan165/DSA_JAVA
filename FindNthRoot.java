193b664a1ec0fe7fb8429b83042d7dafa67fff96
Your Name <ananyam054@gmail.com>
2026-07-28 23:22:32 +0530

    feat: add GFG Find Nth Root using binary search

---
+++ FindNthRoot.java
@@ -0,0 +1,32 @@
+public class FindNthRoot {
+    class Solution {
+    public int nthRoot(int n, int m) {
+
+        if (m == 0)
+            return 0;
+
+        int low = 1;
+        int high = m;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            long power = 1;
+
+            for (int i = 1; i <= n; i++) {
+                power *= mid;
+            }
+
+            if (power == m) {
+                return mid;
+            } else if (power < m) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
+}
